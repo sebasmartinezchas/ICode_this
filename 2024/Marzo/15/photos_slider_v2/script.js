6066c638057fb803050377d94754cc7c0411d84b
sebasmartinezchas <sebastianmartinezchas@gmail.com>
2024-03-15 18:10:44 -0300

    150324 start

---
+++ 2024/Marzo/15/photos_slider_v2/script.js
@@ -0,0 +1,34 @@
+const body = document.body;
+const images = document.querySelectorAll(".image");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
+let activeImage = 0;
+
+rightBtn.addEventListener("click", function () {
+activeImage++;
+
+if (activeImage > images.length - 1) {
+  activeImage = 0;
+}
+setBgToBody();
+setActiveImage();
+});
+leftBtn.addEventListener("click", function () {
+activeImage--;
+
+if (activeImage < 0) {
+activeImage = images.length - 1;
+}
+setBgToBody()
+setActiveImage()
+});
+setBgToBody();
+
+function setBgToBody() {
+body.style.backgroundImage = images[activeImage].style.backgroundImage;
+}
+
+function setActiveImage() {
+images.forEach((image) => image.classList.remove("active"));
+images[activeImage].classList.add("active");
+}
