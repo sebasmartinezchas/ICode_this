20175874a41993e0746665c7068cdfa072fc3a56
sebasmartinezchas <sebasmartinezchas>
2024-03-16 01:02:47 -0300

    150424 finished

--- 2024/Marzo/15/photos_slider_v2/script.js
+++ 2024/Marzo/15/photos_slider_v2/script.js
@@ -2,16 +2,20 @@ const body = document.body;
 const images = document.querySelectorAll(".image");
 const leftBtn = document.getElementById("left");
 const rightBtn = document.getElementById("right");
+const gallery=document.querySelector(".slider__gallery")
 let activeImage = 0;
 
 rightBtn.addEventListener("click", function () {
 activeImage++;
 
+
 if (activeImage > images.length - 1) {
   activeImage = 0;
+
 }
 setBgToBody();
 setActiveImage();
+
 });
 leftBtn.addEventListener("click", function () {
 activeImage--;
@@ -30,5 +34,6 @@ body.style.backgroundImage = images[activeImage].style.backgroundImage;
 
 function setActiveImage() {
 images.forEach((image) => image.classList.remove("active"));
+
 images[activeImage].classList.add("active");
 }
