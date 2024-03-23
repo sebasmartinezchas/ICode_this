46beb8a2d1ad2f843e1e89d41a5b16bedce54234
sebasmartinezchas <sebasmartinezchas>
2024-03-22 23:37:59 -0300

    220324 finish

--- 2024/Marzo/22/progress_bars/script.js
+++ 2024/Marzo/22/progress_bars/script.js
@@ -1,10 +1,10 @@
-const progress = document.getElementById("progress");
-const prev = document.getElementById("prev");
-const next = document.getElementById("next");
-const circles = document.querySelectorAll(".circle");
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const circles = document.querySelectorAll('.circle');
 
 let currentActive = 1;
-next.addEventListener("click", () => {
+next.addEventListener('click', () => {
   currentActive++;
   if (currentActive > circles.length) {
     currentActive = circles.length;
@@ -12,7 +12,7 @@ next.addEventListener("click", () => {
   update();
 });
 
-prev.addEventListener("click", () => {
+prev.addEventListener('click', () => {
   currentActive--;
   if (currentActive < 1) {
     currentActive = 1;
@@ -23,20 +23,20 @@ prev.addEventListener("click", () => {
 function update() {
   circles.forEach((circle, idx) => {
     if (idx < currentActive) {
-      circle.classList.add("active");
+      circle.classList.add('active');
     } else {
-      circle.classList.remove("active");
+      circle.classList.remove('active');
     }
   });
-  const actives = document.querySelectorAll(".active");
-  progress.style.width=(actives.length -1)/(circles.length-1)*100 + '%'
-  if(currentActive===1){
-    prev.disabled=true
-  }else if(currentActive===circles.length){
-    next.disabled=true
-  }else{
-    prev.disabled=false
-    next.disabled=false
+  const actives = document.querySelectorAll('.active');
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
   }
-
 }
